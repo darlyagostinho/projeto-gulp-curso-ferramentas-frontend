40bab1596a3d6b83dd54bb28fe5417597fe5807e
Darly Agostinho <darlyagostinho@gmail.com>
2020-06-07 13:04:13 -0300

    feature-sass work

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+const gulp = require('gulp');
 const gulpsass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const del = require('del');
@@ -6,3 +7,10 @@ const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
 const browsersync = require('browser-sync').create();
 
+function sass(){
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(gulpsass())
+    .pipe(gulp.dest('./src/css'));
+}
+
+module.exports.default = sass;
